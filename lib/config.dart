0f05e5b2d9fae47651bde4c28cf0910702c4eb8c
A'isyah Mawaddah <aisyahmawaddah@graduate.utm.my>
2026-04-07 22:23:49 +0800

    .

--- lib/config.dart
+++ lib/config.dart
@@ -1,6 +1,6 @@
 // lib/config.dart
 class Config {
-  static const String apiUrl = 'https://8072-2a09-bac5-55f9-18be-00-277-6d.ngrok-free.app';
+  static const String apiUrl = 'https://c6f9-2a09-bac6-d69e-15f-00-23-47f.ngrok-free.app';
 }
 
 // class Config {
